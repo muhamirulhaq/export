fc366bdeaeb6c2d99e9c458aecae546869d0144e
Muhammad Amirul Haq <muhammadamirulhaq66@gmail.com>
2020-12-19 14:59:32 +0800

    f

---
+++ coba2.js
@@ -0,0 +1,4 @@
+import {x} from "./modules/coba.js";
+let y = x + 2;
+
+alert(y);
---
+++ modules/coba.js
@@ -0,0 +1,8 @@
+// "use strict";
+
+export function addition(x,y) {
+    let add = x + y;
+    return add;
+}
+
+export let x = 0;
